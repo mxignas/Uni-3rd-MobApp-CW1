aad20ea95769a103a1944fe02d1835e8e3717502
Ignas Lamanauskas <il178@live.mdx.ac.uk>
2020-10-18 14:33:08 +0300

    all activities displayed with add to cart button

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -17,10 +17,10 @@
               <div class="card-body">
                 <h3 class="card-title">{{ lesson.Title }}</h3>
                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">Subject: {{ lesson.Subject }} </li>
+                  <li class="list-group-item">Subject: {{ lesson.Activity }} </li>
                   <li class="list-group-item">Location: {{ lesson.Location }} </li>
                   <li class="list-group-item">Price: {{ lesson.Price }} </li>
-                  <li class="list-group-item">Spaces: {{ lesson.Spaces }} </li>
+                  <li class="list-group-item text-danger">Spaces: {{ lesson.Spaces }} </li>
                 </ul>
               </div>
               <div class="card-body " id="addtocart">
@@ -39,55 +39,99 @@
 <script>
 import mathImg from "../assets/Math.png"
 import englishImg from "../assets/English.png"
-import artImg from "../assets/aRT.png"
+import artImg from "../assets/Art.png"
 import geographyImg from "../assets/Geography.png"
 import scienceImg from "../assets/Science.png"
+import boxingImg from "../assets/Boxing.png"
+import basketballImg from "../assets/Basketball.png"
+import volleyballImg from "../assets/Volleyball.png"
+import tennisImg from "../assets/Tennis.png"
+import soccerImg from "../assets/Soccer.png"
 
 export default {
   name: 'Index',
   data () {
     return {
-      LessonTitle: "Lesson",
-      ActivityTitle: "Activity",
       Lessons: [
         {
           url: mathImg,
-          Title: "Lesson",
-          Subject: "Math",
-          Location: "London",
+          Title: "Lessons",
+          Activity: "Math",
+          Location: "West End",
           Price: "£100",
           Spaces: 5,
         },
         {
+          url: soccerImg,
+          Title: "Sports",
+          Activity: "Soccer",
+          Location: "Hendon",
+          Price: "£50",
+          Spaces: 5,
+        },
+        {
           url: englishImg,
-          Title: "Lesson",
-          Subject: "English",
-          Location: "London",
+          Title: "Lessons",
+          Activity: "English",
+          Location: "South Kensington",
           Price: "£80",
+          Spaces: 5,
+        },
+        {
+          url: boxingImg,
+          Title: "Sports",
+          Activity: "Boxing",
+          Location: "Hendon",
+          Price: "£60",
           Spaces: 5,
         },
         {
           url: artImg,
-          Title: "Lesson",
-          Subject: "Art",
+          Title: "Lessons",
+          Activity: "Art",
           Location: "London",
           Price: "£80",
           Spaces: 5,
         },
         {
+          url: tennisImg,
+          Title: "Sports",
+          Activity: "Tennis",
+          Location: "Westminster",
+          Price: "£150",
+          Spaces: 5,
+        },
+        {
           url: scienceImg,
-          Title: "Lesson",
-          Subject: "Science",
+          Title: "Lessons",
+          Activity: "Science",
           Location: "London",
           Price: "£120",
           Spaces: 5,
         },
         {
+          url: volleyballImg,
+          Title: "Sports",
+          Activity: "Volleyball",
+          Location: "Camden Town",
+          Price: "£50",
+          Spaces: 5,
+        },
+
+        {
           url: geographyImg,
-          Title: "Lesson",
-          Subject: "Geography",
+          Title: "Lessons",
+          Activity: "Geography",
           Location: "London",
           Price: "£60",
+          Spaces: 5,
+        },
+        {
+          url: basketballImg,
+          Title: "Sports",
+          Activity: "Basketball",
+          Location: "Soho",
+          Price: "£95",
           Spaces: 5,
         },
         ],
@@ -104,6 +148,7 @@ export default {
     margin-bottom: 10px;
   }
   #addtocart {
-    margin-left: 130px;
+    margin-left: 100px;
+    width: 150px;
   }
 </style>
