888cabeadb4ab65c8b703a52316b2e57db207189
Ignas Lamanauskas <il178@live.mdx.ac.uk>
2020-10-22 18:51:49 +0300

    adding to cart, spaces reducing by rpessing, button not showing until smth in the cart

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="MainPage">
-
-    <nav aria-label="breadcrumb" id="bread">
+    <!-- <nav aria-label="breadcrumb" id="bread">
       <ol class="breadcrumb">
         <li class="breadcrumb-item active" aria-current="page">Home</li>
+        <li class="breadcrumb-item align-self-end"> Checkout</li>
       </ol>
-    </nav>
+    </nav> -->
+
+    <div class="d-flex bg-secondary">
+      <div class="p-2 bg-info">Home</div>
+      <div class="ml-auto p-2" v-if="cart.length > 0">Cart: {{ cart.length }}</div>
+    </div>
+
 
     <div class="container bg-light text-muted">
       <div class="row">
@@ -13,12 +19,14 @@
         <div class="sorting-div">
           <h4>Sort by:</h4>
           <div class="btn-group-vertical btn-group-toggle" data-toggle="buttons">
-            <label class="btn btn-secondary" v-for="blist in ByList" :key="blist.id" @click="addingClass">
+            <label class="btn btn-secondary" v-for="blist in ByList" :key="blist.id" @:click="sortBy(blist.label)">
               <input type="radio" name="options" id="Subject" autocomplete="off" checked> {{ blist.label }}
             </label>
           </div>
+
             <br>
             <p></p>
+
           <div class="btn-group-vertical btn-group-toggle" data-toggle="buttons">
             <label class="btn btn-secondary" v-for="hlist in HowList" :key="hlist.id">
               <input type="radio" name="options" id="Ascending" autocomplete="off" checked> {{ hlist.label }}
@@ -26,7 +34,7 @@
           </div>
         </div>
 
-        <div class="col-md-6 col-lg-3" v-for="lesson in Lessons" :key="lesson.id">
+        <div class="col-md-6 col-lg-3" v-for="(lesson, index) in Lessons" :key="lesson.index">
         
           <div class="card" >
             <img :src="lesson.url" alt="" class="card-img-top img-fluid">
@@ -36,11 +44,11 @@
                   <li class="list-group-item">Subject: {{ lesson.Activity }} </li>
                   <li class="list-group-item">Location: {{ lesson.Location }} </li>
                   <li class="list-group-item">Price: {{ lesson.Price }} </li>
-                  <li class="list-group-item text-danger">Spaces: {{ lesson.Spaces }} </li>
+                  <li class="list-group-item text-danger">Availability: {{ lesson.Availability }} </li>
                 </ul>
               </div>
               <div class="card-body " id="addtocart">
-                <button type="button" class="btn btn-info" >Add To Cart</button>
+                <button type="button" class="btn btn-info" @click='addToCart(index)' >Add To Cart</button>
               </div>
           </div>
 
@@ -68,53 +76,55 @@ export default {
   name: 'Index',
   data () {
     return {
+      cart: [],
       ByList: [
-        { label: "Subject", active: false
-        },
-        { label: "Location", active: false
-        },
-        { label: "Price", active: false
-        },
-        { label: "Availability", active: false
-        }
+        { label: "Subject"},
+        { label: "Location"},
+        { label: "Price"},
+        { label: "Availability"}
       ],
       HowList: [
-        { label: "Ascending", active: false
-        },
-        { label: "Descending", active: false
-        },
+        { label: "Ascending"},
+        { label: "Descending"},
       ],
       Lessons: [
-        {
-          url: mathImg, Title: "Lessons", Activity: "Math", Location: "West End", Price: "£100", Spaces: 5,
-        },
-        { url: soccerImg, Title: "Sports", Activity: "Soccer", Location: "Hendon", Price: "£50", Spaces: 5,
-        },
-        { url: englishImg, Title: "Lessons", Activity: "English", Location: "Kensington", Price: "£80", Spaces: 5,
-        },
-        { url: boxingImg, Title: "Sports", Activity: "Boxing", Location: "Hendon", Price: "£60", Spaces: 5,
-        },
-        { url: artImg, Title: "Lessons", Activity: "Art",  Location: "London", Price: "£80", Spaces: 5,
-        },
-        { url: tennisImg, Title: "Sports", Activity: "Tennis", Location: "Westminster", Price: "£150", Spaces: 5,
-        },
-        { url: scienceImg, Title: "Lessons", Activity: "Science", Location: "London", Price: "£120", Spaces: 5,
-        },
-        { url: volleyballImg, Title: "Sports", Activity: "Volleyball", Location: "Camden Town", Price: "£50", Spaces: 5,
-        },
-        { url: geographyImg, Title: "Lessons", Activity: "Geography", Location: "London", Price: "£60", Spaces: 5,
-        },
-        { url: basketballImg, Title: "Sports",  Activity: "Basketball", Location: "Soho", Price: "£95", Spaces: 5,
-        },
+        { id: 1001, url: mathImg, Title: "Lessons", Activity: "Math", Location: "West End", Price: "100", Availability: 5 },
+        { id: 1002, url: soccerImg, Title: "Sports", Activity: "Soccer", Location: "Hendon", Price: "50", Availability: 5 },
+        { id: 1003, url: englishImg, Title: "Lessons", Activity: "English", Location: "Kensington", Price: "80", Availability: 5 },
+        { id: 1004, url: boxingImg, Title: "Sports", Activity: "Boxing", Location: "Hendon", Price: "60", Availability: 5 },
+        { id: 1005, url: artImg, Title: "Lessons", Activity: "Art",  Location: "London", Price: "80", Availability: 5 },
+        { id: 1006, url: tennisImg, Title: "Sports", Activity: "Tennis", Location: "Westminster", Price: "150", Availability: 5 },
+        { id: 1007, url: scienceImg, Title: "Lessons", Activity: "Science", Location: "London", Price: "120", Availability: 5 },
+        { id: 1008, url: volleyballImg, Title: "Sports", Activity: "Volleyball", Location: "Camden Town", Price: "50", Availability: 5 },
+        { id: 1009, url: geographyImg, Title: "Lessons", Activity: "Geography", Location: "London", Price: "60", Availability: 5 },
+        { id: 1010, url: basketballImg, Title: "Sports",  Activity: "Basketball", Location: "Soho", Price: "95", Availability: 5 },
         ],
     }
   },
   methods: {
-    addingClass: function() {
-      this.ByList.addClass("active")
+    // addingClass() {
+    //   this.ByList.addClass("active")
+    // },
+    sortBy() {
+      this.Lessons.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
+    },
+    addToCart(index) {
+      this.cart.push(this.Lessons[index].id);
+      console.log(this.Lessons[index].id)
+      this.Lessons[index].Availability = this.Lessons[index].Availability - 1
+      console.log(this.cart.length)
     }
+  },
+  computed: {
+    cartItemNumber: function() {
+      return this.cart.length || '';
+    },
+    ifCanAdd: function() {
+      return this.Lessons[index].Availability > this.cartItemNumber
+    },
+
   }
-  }
+}
 
 </script>
 
