53377f0328847374d1310dfde1ad4301187f732d
Ignas Lamanauskas <il178@live.mdx.ac.uk>
2020-11-10 18:19:46 +0200

    Trying sorting with vuex mutations

--- src/store.js
+++ src/store.js
@@ -25,19 +25,35 @@ export const store = new Vuex.Store({
             email: '',
         },
         Lessons: [
-        { id: 1001, url: mathImg, Title: "Lessons", Activity: "Math", Location: "West End", Price: "100", Availability: 5 },
-        { id: 1002, url: soccerImg, Title: "Sports", Activity: "Soccer", Location: "Hendon", Price: "50", Availability: 5 },
-        { id: 1003, url: englishImg, Title: "Lessons", Activity: "English", Location: "Kensington", Price: "80", Availability: 5 },
-        { id: 1004, url: boxingImg, Title: "Sports", Activity: "Boxing", Location: "Hendon", Price: "60", Availability: 5 },
-        { id: 1005, url: artImg, Title: "Lessons", Activity: "Art", Location: "London", Price: "80", Availability: 5 },
-        { id: 1006, url: tennisImg, Title: "Sports", Activity: "Tennis", Location: "Westminster", Price: "150", Availability: 5 },
-        { id: 1007, url: scienceImg, Title: "Lessons", Activity: "Science", Location: "London", Price: "120", Availability: 5 },
-        { id: 1008, url: volleyballImg, Title: "Sports", Activity: "Volleyball", Location: "Camden Town", Price: "50", Availability: 5 },
-        { id: 1009, url: geographyImg, Title: "Lessons", Activity: "Geography", Location: "London", Price: "60", Availability: 5 },
-        { id: 1010, url: basketballImg, Title: "Sports", Activity: "Basketball", Location: "Soho", Price: "95", Availability: 5 },
+        { id: 1001, url: mathImg, Title: "Lessons", Activity: "Math", Location: "West End", Price: 100, Availability: 5 },
+        { id: 1002, url: soccerImg, Title: "Sports", Activity: "Soccer", Location: "Hendon", Price: 50, Availability: 5 },
+        { id: 1003, url: englishImg, Title: "Lessons", Activity: "English", Location: "Kensington", Price: 80, Availability: 5 },
+        { id: 1004, url: boxingImg, Title: "Sports", Activity: "Boxing", Location: "Hendon", Price: 60, Availability: 5 },
+        { id: 1005, url: artImg, Title: "Lessons", Activity: "Art", Location: "London", Price: 80, Availability: 5 },
+        { id: 1006, url: tennisImg, Title: "Sports", Activity: "Tennis", Location: "Westminster", Price: 150, Availability: 5 },
+        { id: 1007, url: scienceImg, Title: "Lessons", Activity: "Science", Location: "London", Price: 120, Availability: 5 },
+        { id: 1008, url: volleyballImg, Title: "Sports", Activity: "Volleyball", Location: "Camden Town", Price: 50, Availability: 5 },
+        { id: 1009, url: geographyImg, Title: "Lessons", Activity: "Geography", Location: "London", Price: 60, Availability: 5 },
+        { id: 1010, url: basketballImg, Title: "Sports", Activity: "Basketball", Location: "Soho", Price: 95, Availability: 5 },
         ],
         cart: [],
-    }
+    },
+    mutations: {
+        SORT_LESSONS_BY_TITLE(state) {
+            if(state.Lessons > 0) {
+            state.Lessons.Title.sort(function (a, b) {
+                let titleA = a.Title.toUpperCase()
+                let titleB = b.Title.toUpperCase()
+                if (titleA < titleB) {
+                    return -1
+                }
+                if (titleA > titleB) {
+                    return 1
+                }
+            })
+    }}
+}
 })
+
 
 
